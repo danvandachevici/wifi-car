bc6c15733ec634a78834215420bf15aba29d4704
Dan VANDACHEVICI <dan@vandachevici.ro>
2021-12-19 11:24:03 +0100

    Optimizations

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const exec = require('child_process').execSync;
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const DIRECTIONS = {
+  forward: {left: 1, right: 1},
+  backward: {left: -1, right: -1},
+  left: {left: -1, right: 1},
+  right: {left: 1, right: -1}
+};
+const MOTORS = {
+	left: 0,
+	right: 1
+};
 
 app.use(express.static('site'));
 
-const path = require('path');
+// go back
+control(DIRECTIONS.backward);
+setTimeout(() => {
+  stop();
+}, 3000);
 
 try {
 	exec('omega2-ctrl gpiomux set pwm0 pwm');
@@ -22,71 +37,19 @@ app.get('/api/:path', (req, res) => {
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
-
-const DIRECTIONS = {
-  forward: 0,
-  backward: 1,
-  left: 2,
-  right: 3
-}
-const MOTORS = {
-	left: 0,
-	right: 1
-}
-const speed = 100;
-
-var keypress = require('keypress');
-//init();
-
-const speedLevels = {
-    left: 0,
-    right: 0
-};
-const increment = 70;
+});
 
 function takeAction(action) {
     switch(action) {
-		case 'up': run(DIRECTIONS.forward); break;
-		case 'left': run(DIRECTIONS.left); break;
-		case 'right': run(DIRECTIONS.right); break;
-		case 'down': run(DIRECTIONS.backward); break;
+		case 'up': control(DIRECTIONS.forward); break;
+		case 'left': control(DIRECTIONS.left); break;
+		case 'right': control(DIRECTIONS.right); break;
+		case 'down': control(DIRECTIONS.backward); break;
 		default: stop();
     }
 }
 
-//function init() {
-//	// make `process.stdin` begin emitting "keypress" events
-//	keypress(process.stdin);
-//
-//
-//	process.stdin.setRawMode(true);
-//	process.stdin.resume();
-//
-//	process.stdin.on('keypress', function (ch, key) {
-//  		console.log('got "keypress"', key.name);
-//		switch(key.name) {
-//			case 'up': run(DIRECTIONS.forward); break;
-//			case 'left': run(DIRECTIONS.left); break;
-//			case 'right': run(DIRECTIONS.right); break;
-//			case 'down': run(DIRECTIONS.backward); break;
-//			default: stop();
-//		}
-//  		if (key && key.ctrl && key.name == 'c') {
-//    		process.stdin.pause();
-//  		}
-//	});
-//
-//
-//	console.log('Everything is up and running');
-//}
-//
-//
-const speed = 70;
 function control(params) {
-
-	// const goBackPinLeft = 1;
-	// const goBackPinRight = 0;
 
 	const frequency = 50;
 	const dutyCycle = 60;
@@ -95,42 +58,40 @@ function control(params) {
 
 	const mapping = {
 		left: [
-			`fast-gpio pwm ${MOTORS.left} ${frequency} ${dutyCycle}`,
-			`onion pwm ${MOTORS.left} 0 50 && fast-gpio set ${MOTORS.left} 0`,
-			`onion pwm ${MOTORS.left} ${dutyCycle} ${frequency}`,
+      // BACKWARD
+			`onion pwm ${MOTORS.left} 0 ${frequency} && fast-gpio pwm ${MOTORS.left} ${frequency} ${dutyCycle}`,
+      // STOP
+			`onion pwm ${MOTORS.left} 0 ${frequency} && fast-gpio set ${MOTORS.left} 0`,
+      // FORWARD
+			`fast-gpio set ${MOTORS.left} 0 && onion pwm ${MOTORS.left} ${dutyCycle} ${frequency}`,
 		],
 		right: [
-			`fast-gpio pwm ${MOTORS.right} ${frequency} ${dutyCycle}`,
-			`onion pwm ${MOTORS.right} 0 50 && fast-gpio set ${MOTORS.right} 0`,
-			`onion pwm ${MOTORS.right} ${dutyCycle} ${frequency}`,
+      // BACKWARD
+			`onion pwm ${MOTORS.right} 0 ${frequency} && fast-gpio pwm ${MOTORS.right} ${frequency} ${dutyCycle}`,
+      // STOP
+			`onion pwm ${MOTORS.right} 0 ${frequency} && fast-gpio set ${MOTORS.right} 0`,
+      // FORWARD
+			`fast-gpio set ${MOTORS.right} 0 && onion pwm ${MOTORS.right} ${dutyCycle} ${frequency}`,
 		]
 	};
-	commands.push(mapping.left[params.left+1]);
-	commands.push(mapping.right[params.right+1]);
 
-	commands.forEach((cmd) => {
-		try {
-			exec(cmd);
-		} catch(e) {
-			console.log(`can't execute cmd:`, cmd);
-		}
-	})
+  const cmd = `${mapping.left[params.left+1]} && ${mapping.right[params.right+1]}`;
+
+	console.log('Commands', cmd);
+
+  try {
+    exec(cmd);
+  } catch(e) {
+    console.log(`can't execute cmd:`, cmd);
+  }
 }
-function run(direction) {
-	switch (direction) {
-		case DIRECTIONS.right: {control({right: -1, left: 1}); break;}
-		case DIRECTIONS.left: {control({right: 1, left: -1}); break;}
-		case DIRECTIONS.forward: {control({right: 1, left: 1}); break;}
-		case DIRECTIONS.backward: {control({right: -1, left: -1}); return;}
-		default: {control({right: 0, left: 0})}
-	}
-}
+
 function stop() {
-	speedLevels.left = 0;
-	speedLevels.right = 0;
 	try {
-		exec('onion pwm 0 0 50');
-		exec('onion pwm 1 0 50');
+		exec(`onion pwm ${MOTORS.left} 0 50`);
+		exec(`onion pwm ${MOTORS.right} 0 50`);
+    exec(`fast-gpio set ${MOTORS.left} 0`);
+    exec(`fast-gpio set ${MOTORS.right} 0`);
 	} catch(e) {
 		console.log('Can not stop:', e);
 	}
